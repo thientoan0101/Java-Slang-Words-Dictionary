7f21d9b0d60ed51247587d8481829e3f7565d687
thientoan0101 <thientoancubi@gmail.com>
2021-12-19 14:48:29 +0700

    init commit
    init with main.java

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company;
+
+/**
+ *
+ * @author thien
+ */
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("hello");
+    }
+}
